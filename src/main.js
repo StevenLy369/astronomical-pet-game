23d3aaa31a016ab2937ed3178f338dad7458ab22
brevin <cronkbrevin98@gmail.com>
2020-04-21 10:17:48 -0700

    UI polish

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import './styles.css';
 import $ from 'jquery';
 import { Alien } from '../src/alien';
 
-
+// const urlFun = "api.giphy.com/v1/gifs/pHYLxGVWf4J4DmS6k6?q=cute+cartoon+dog&api_key=WZ6j341npaXaXQsbdICWWvyAE14rr9lY";
+// const urlBandaid = "http://api.giphy.com/v1/gifs/1itJk0YC7kUYF6yv70?q=cute+cartoon+bandaid&api_key=5a0es7p3EfdqyxbDjaa2K7vQEL5m7V7i";
+// const urlBathroom = "http://api.giphy.com/v1/gifs/2wS9xmYJNsFUTzcNKU?q=cute+bathroom&api_key=5a0es7p3EfdqyxbDjaa2K7vQEL5m7V7i";
+// const urlFood = "http://api.giphy.com/v1/gifs/mB9gnpgb6q2TLAqOrW?q=cute+cake&api_key=5a0es7p3EfdqyxbDjaa2K7vQEL5m7V7i";
+// const urlBed = "http://api.giphy.com/v1/gifs/jp8ubVlWwJ8w8?q=cute+cartoon+bed&api_key=5a0es7p3EfdqyxbDjaa2K7vQEL5m7V7i"
 
 
 
